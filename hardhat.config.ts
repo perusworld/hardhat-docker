a5bab7c2cb0aceb104bb1e59b67eb931e17be50c
Saravana Perumal Shanmugam <perusworld@linux.com>
2025-08-11 20:44:32 -0700

    upgr
    Signed-off-by: Saravana Perumal Shanmugam <perusworld@linux.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.28",
   networks: {
     hardhat: {
       chainId: parseInt(process.env.CHAIN_ID || '31337'),
